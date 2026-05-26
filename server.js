fa74284da8f6b2ba089fb98f6746852b2dff6df3
parmodaonla <parmodaonla@gmail.com>
2026-05-26 15:21:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Tockk Profile API Running");
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+   console.log("MongoDB Connected");
+})
+.catch((err) => {
+   console.log(err);
+});
+
+app.get("/", (req,res)=>{
+   res.send("Tockk Profile API Running");
 });
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(PORT, ()=>{
+   console.log(`Server running on ${PORT}`);
 });
