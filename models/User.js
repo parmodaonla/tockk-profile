ab7d284f251323cb8f2ed79505ce06d5c2ceb0a6
parmodaonla <parmodaonla@gmail.com>
2026-05-28 10:03:35 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,48 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  phone: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  otp: {
+    type: String,
+    default: ""
+  },
+
+  otpVerified: {
+    type: Boolean,
+    default: false
+  },
+
+  profilePic: {
+    type: String,
+    default: ""
+  },
+
+  bio: {
+    type: String,
+    default: ""
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
