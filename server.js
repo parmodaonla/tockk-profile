3a9961ba7c0d5ff4c9717db49b21994d6c121fa1
parmodaonla <parmodaonla@gmail.com>
2026-05-28 10:33:09 +0530

    Add CORS support and organize routes in server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+
+// AUTH ROUTES
+app.use("/api/auth", authRoutes);
+
+
+// MONGODB CONNECTION
 mongoose.connect(process.env.MONGO_URI, {
   serverSelectionTimeoutMS: 10000
 })
@@ -16,12 +27,16 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log("Mongo Error:", err.message);
 });
 
-app.get("/", (req,res)=>{
+
+// HOME ROUTE
+app.get("/", (req, res) => {
   res.send("Tockk Profile API Running");
 });
 
+
+// SERVER
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
